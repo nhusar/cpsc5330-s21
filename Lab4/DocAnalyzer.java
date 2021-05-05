2d12f1538530640a47c8f7bf2cec43b28ea7b880
Nataliya Husar <nhusar@gmail.com>
2021-05-04 17:13:25 -0700

    final working version with static array of stopwords

--- Lab4/DocAnalyzer.java
+++ Lab4/DocAnalyzer.java
@@ -1,7 +1,5 @@
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.FileReader;
-
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,7 +17,54 @@ import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 
 public class DocAnalyzer {
 
-  private static final String STOPWORDS_FILE = "stopwords.txt";
+  private static final HashSet<String> stopwords = new HashSet<>(Arrays.asList("a", "as", "able", "about", "above",
+      "according", "accordingly", "across", "actually", "after", "afterwards", "again", "against", "aint", "all",
+      "allow", "allows", "almost", "alone", "along", "already", "also", "although", "always", "am", "among", "amongst",
+      "an", "and", "another", "any", "anybody", "anyhow", "anyone", "anything", "anyway", "anyways", "anywhere",
+      "apart", "appear", "appreciate", "appropriate", "are", "arent", "around", "as", "aside", "ask", "asking",
+      "associated", "at", "available", "away", "awfully", "be", "became", "because", "become", "becomes", "becoming",
+      "been", "before", "beforehand", "behind", "being", "believe", "below", "beside", "besides", "best", "better",
+      "between", "beyond", "both", "brief", "but", "by", "cmon", "cs", "came", "can", "cant", "cannot", "cant", "cause",
+      "causes", "certain", "certainly", "changes", "clearly", "co", "com", "come", "comes", "concerning",
+      "consequently", "consider", "considering", "contain", "containing", "contains", "corresponding", "could",
+      "couldnt", "course", "currently", "definitely", "described", "despite", "did", "didnt", "different", "do", "does",
+      "doesnt", "doing", "dont", "done", "down", "downwards", "during", "each", "edu", "eg", "eight", "either", "else",
+      "elsewhere", "enough", "entirely", "especially", "et", "etc", "even", "ever", "every", "everybody", "everyone",
+      "everything", "everywhere", "ex", "exactly", "example", "except", "far", "few", "ff", "fifth", "first", "five",
+      "followed", "following", "follows", "for", "former", "formerly", "forth", "four", "from", "further",
+      "furthermore", "get", "gets", "getting", "given", "gives", "go", "goes", "going", "gone", "got", "gotten",
+      "greetings", "had", "hadnt", "happens", "hardly", "has", "hasnt", "have", "havent", "having", "he", "hes",
+      "hello", "help", "hence", "her", "here", "heres", "hereafter", "hereby", "herein", "hereupon", "hers", "herself",
+      "hi", "him", "himself", "his", "hither", "hopefully", "how", "howbeit", "however", "i", "id", "ill", "im", "ive",
+      "ie", "if", "ignored", "immediate", "in", "inasmuch", "inc", "indeed", "indicate", "indicated", "indicates",
+      "inner", "insofar", "instead", "into", "inward", "is", "isnt", "it", "itd", "itll", "its", "its", "itself",
+      "just", "keep", "keeps", "kept", "know", "knows", "known", "last", "lately", "later", "latter", "latterly",
+      "least", "less", "lest", "let", "lets", "like", "liked", "likely", "little", "look", "looking", "looks", "ltd",
+      "mainly", "many", "may", "maybe", "me", "mean", "meanwhile", "merely", "might", "more", "moreover", "most",
+      "mostly", "much", "must", "my", "myself", "name", "namely", "nd", "near", "nearly", "necessary", "need", "needs",
+      "neither", "never", "nevertheless", "new", "next", "nine", "no", "nobody", "non", "none", "noone", "nor",
+      "normally", "not", "nothing", "novel", "now", "nowhere", "obviously", "of", "off", "often", "oh", "ok", "okay",
+      "old", "on", "once", "one", "ones", "only", "onto", "or", "other", "others", "otherwise", "ought", "our", "ours",
+      "ourselves", "out", "outside", "over", "overall", "own", "particular", "particularly", "per", "perhaps", "placed",
+      "please", "plus", "possible", "presumably", "probably", "provides", "que", "quite", "qv", "rather", "rd", "re",
+      "really", "reasonably", "regarding", "regardless", "regards", "relatively", "respectively", "right", "said",
+      "same", "saw", "say", "saying", "says", "second", "secondly", "see", "seeing", "seem", "seemed", "seeming",
+      "seems", "seen", "self", "selves", "sensible", "sent", "serious", "seriously", "seven", "several", "shall", "she",
+      "should", "shouldnt", "since", "six", "so", "some", "somebody", "somehow", "someone", "something", "sometime",
+      "sometimes", "somewhat", "somewhere", "soon", "sorry", "specified", "specify", "specifying", "still", "sub",
+      "such", "sup", "sure", "ts", "take", "taken", "tell", "tends", "th", "than", "thank", "thanks", "thanx", "that",
+      "thats", "thats", "the", "their", "theirs", "them", "themselves", "then", "thence", "there", "theres",
+      "thereafter", "thereby", "therefore", "therein", "theres", "thereupon", "these", "they", "theyd", "theyll",
+      "theyre", "theyve", "think", "third", "this", "thorough", "thoroughly", "those", "though", "three", "through",
+      "throughout", "thru", "thus", "to", "together", "too", "took", "toward", "towards", "tried", "tries", "truly",
+      "try", "trying", "twice", "two", "un", "under", "unfortunately", "unless", "unlikely", "until", "unto", "up",
+      "upon", "us", "use", "used", "useful", "uses", "using", "usually", "value", "various", "very", "via", "viz", "vs",
+      "want", "wants", "was", "wasnt", "way", "we", "wed", "well", "were", "weve", "welcome", "well", "went", "were",
+      "werent", "what", "whats", "whatever", "when", "whence", "whenever", "where", "wheres", "whereafter", "whereas",
+      "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whos", "whoever",
+      "whole", "whom", "whose", "why", "will", "willing", "wish", "with", "within", "without", "wont", "wonder",
+      "would", "would", "wouldnt", "yes", "yet", "you", "youd", "youll", "youre", "youve", "your", "yours", "yourself",
+      "yourselves", "zero"));
 
   public static class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
 
@@ -31,7 +76,7 @@ public class DocAnalyzer {
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
       final String fileName = ((FileSplit) context.getInputSplit()).getPath().getName().replaceAll(".txt", "");
       outDocId.set(fileName);
-      for (Matcher match = wordPattern.matcher(value.toString().toLowerCase()); match.find(); ) {
+      for (Matcher match = wordPattern.matcher(value.toString().toLowerCase()); match.find();) {
         outToken.set(match.group());
         context.write(outDocId, outToken);
       }
@@ -40,16 +85,7 @@ public class DocAnalyzer {
 
   public static class TokenCategoryReducer extends Reducer<Text, Text, Text, Text> {
 
-    private final HashSet<String> stopwords = new HashSet<>();
     private final Text outStats = new Text();
-
-    public TokenCategoryReducer() throws IOException {
-      try (final BufferedReader reader = new BufferedReader(new FileReader(STOPWORDS_FILE))) {
-        for (String line; (line = reader.readLine()) != null; ) {
-          stopwords.add(line.trim().toLowerCase());
-        }
-      }
-    }
 
     public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
       int countTokens = 0;
@@ -72,9 +108,9 @@ public class DocAnalyzer {
           }
         }
       }
-      outStats.set(String.format("%s\t%d\t%d\t%d\t%.4f\t%s",
-        key, countTokens, countStopwords, uniqueTerms.size(),
-        totalWordLen / Math.max(1.0, countWords), wordLongest));
+      outStats.set(String.format("%d\t%d\t%d\t%.4f\t%s",
+          countTokens, countStopwords, uniqueTerms.size(),
+          totalWordLen / Math.max(1.0, countWords), wordLongest));
       context.write(key, outStats);
     }
   }
@@ -82,7 +118,6 @@ public class DocAnalyzer {
   public static void main(String[] args) throws Exception {
 
     Configuration conf = new Configuration();
-    conf.addResource(new Path(STOPWORDS_FILE));
 
     Job job = Job.getInstance(conf, "DocAnalyzer");
 
